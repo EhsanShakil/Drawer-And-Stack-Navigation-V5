b688423837da4a41099d1dfb54b952cbe03a8dc4
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-03-21 23:49:11 +0500

    Updating

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './src/navigation/Drawer';
 import StackNavigator from './src/navigation/Stack';
+import Home from './src/screens/Home';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <DrawerNavigator />
+    // </NavigationContainer>
+    <Home />
   );
 };
 
--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import Dots from 'react-native-vector-icons/dist/Entypo';
+import Logout from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 
 const Header = () => {
   return (
-    <View>
-      <TouchableOpacity
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        }}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Header</Text>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <TouchableOpacity>
+        <Dots name="dots-three-vertical" size={30} color="white" />
+      </TouchableOpacity>
+      <Text style={{fontSize: 18, fontWeight: 'bold'}}>Header</Text>
+      <TouchableOpacity>
+        <Logout name="dots-three-vertical" size={30} color="white" />
       </TouchableOpacity>
     </View>
   );
